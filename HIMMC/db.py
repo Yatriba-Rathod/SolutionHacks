c262612e339869e42729e4c322a99635abfe3be6
Yatri Rathod <91048369+Yatriba-Rathod@users.noreply.github.com>
2025-07-16 22:05:54 -0400

    Update db.py

--- HIMMC/db.py
+++ HIMMC/db.py
@@ -2,7 +2,7 @@ from pymongo import MongoClient
 import os
 
 # Connect to MongoDB
-MONGO_URI = "mongodb+srv://harshivpgajjar:z7ZanbB2zL3QOkpc@cluster0.3fsmzlv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"  # e.g., "mongodb+srv://username:password@cluster0.mongodb.net/?retryWrites=true&w=majority"
+MONGO_URI =   # e.g., "mongodb+srv://username:password@cluster0.mongodb.net/?retryWrites=true&w=majority"
 
 client = MongoClient(os.getenv("MONGO_URI"))
 
